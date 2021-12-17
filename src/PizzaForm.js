a4a462c023c4c624d59b0da7bd4f718eab7537c8
kylerostler <93017344+kylerostler@users.noreply.github.com>
2021-12-17 10:57:26 -0700

    started working on form

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,5 +1,19 @@
+import React from 'react';
 
 
 export default function PizzaForm () {
 
+    return (
+        <form onSubmit={onSubmit} id="pizza-form">
+            <div>
+                <input
+                    name="name-input"
+                    type="text"
+                    placeholder="pizza here"
+                    maxLength="30"
+                    onChange={onChange}
+                />
+            </div>
+        </form>
+    )
 }
--- src/PizzaMenu.js
+++ src/PizzaMenu.js
@@ -1,4 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function PizzaMenu () {
 
+    return (
+        <div>
+            <Link to ={`/pizza`}>
+            <button id="order-pizza">Order Pizza</button>
+            </Link>
+        </div>
+    )
 }
