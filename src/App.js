12c61abf420a77f0b08386efff6171613e650a58
kylerostler <93017344+kylerostler@users.noreply.github.com>
2021-12-17 10:37:16 -0700

    started building routes and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
+
+import PizzaMenu from './PizzaMenu';
+import PizzaForm from './PizzaForm'
 
 const App = () => {
+
+
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <h1>BloomTech Eats</h1>
+      <p>Get Your Pizza! Tasty Pizza!</p>
+      <Switch>
+        <Route path="/pizza">
+          <PizzaForm />
+        </Route>
+        
+        <Route path="/">
+          <PizzaMenu />
+        </Route>
+      </Switch>
     </>
   );
 };
